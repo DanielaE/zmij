5cfef5abf392b3b899670acb8c7ad7a65e55aaff
Daniela Engert <dani@ngrt.de>
2025-12-21 19:27:32 +0100

    avoid ODR violations by augmenting the header
    namespace-scope `constexpr` variables have external linkage, but are *not* implicitly `inline`. To avoid multiple copies in a program (i.e. when `zmij.h` happens to be included into multiple TUs), make `zmij::buffer_size` an inline variable.
    
    On top of that, introduce an include guard to avoid multiple inclusions of `zmij.h` into the same TU. Otherwise, `zmij::buffer_size` would be *defined* multiple times.

--- zmij.h
+++ zmij.h
@@ -3,12 +3,17 @@
 // Distributed under the MIT license (see LICENSE) or alternatively
 // the Boost Software License, Version 1.0.
 
+#ifndef ZMIJ_H_INCLUDED
+#define ZMIJ_H_INCLUDED
+
 namespace zmij {
 
-constexpr int buffer_size = 25;
+constexpr inline int buffer_size = 25;
 
 /// Writes the shortest correctly rounded decimal representation of `value` to
 /// `buffer`. `buffer` should point to a buffer of size `buffer_size` or larger.
 void dtoa(double value, char* buffer) noexcept;
 
 }  // namespace zmij
+
+#endif
